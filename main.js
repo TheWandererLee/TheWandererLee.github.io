8c3cd8d13796d21471be8254922f93b887b0eb2a
Lee <usbroforce@gmail.com>
2019-05-18 02:07:21 -0400

    Div blocks, removed broken Array.from()

--- main.js
+++ main.js
@@ -1,49 +1,48 @@
 (() => {
     const Board = {
-        filledBlock: '&#x25A0;',
-        emptyBlock: '&#x25A1;',
+        blockElement: document.createElement('div'),
         sizeInPixels: [320, 640],
         size: [10, 20],
         currentPosition: [0, 0]
     }
 
-    const Pieces = [
-        {
+    const Pieces = {
+        I: {
             name: 'I',
             data: [[0,0],[0,1],[0,2],[0,3]],
             color: '#0EE'
         },
-        {
+        O: {
             name: 'O',
             data: [[0,0],[0,1],[1,0],[1,1]],
             color: '#EE0'
         },
-        {
+        T: {
             name: 'T',
             data: [[1,0],[0,1],[1,1],[2,1]],
             color: '#90E'
         },
-        {
+        S: {
             name: 'S',
             data: [[0,1],[1,1],[1,0],[2,0]],
             color: '#0E0'
         },
-        {
+        Z: {
             name: 'Z',
             data: [[0,0],[1,0],[1,1],[2,1]],
             color: '#E00'
         },
-        {
+        J: {
             name: 'J',
             data: [[1,0],[1,1],[1,2],[0,2]],
             color: '#00E'
         },
-        {
+        L: {
             name: 'L',
             data: [[0,0],[0,1],[0,2],[1,2]],
             color: '#E90'
         }
-    ];
+    };
 
     window.addEventListener('load', () => {
         Board.element = document.getElementById('board');
@@ -51,7 +50,7 @@
         Board.style();
         initGrid();
 
-        createPiece(5, 2);
+        createPiece(3, 2);
         
         drawBoard();
     })
@@ -61,43 +60,69 @@
         Board.element.style.height = Board.sizeInPixels[1] + "px";
         Board.element.style.backgroundColor = "#DDD";
 
-        // Determine appropriate font size for blocks
-        Board.element.style.fontSize = (Board.sizeInPixels[0] / Board.size[0]) + 'px';
-        Board.element.style.lineHeight = (Board.sizeInPixels[1] / Board.size[1]) + 'px';
+        // Determine appropriate size for blocks
+        Board.blockElement.style.width = (Board.sizeInPixels[0] / Board.size[0]) + "px";
+        Board.blockElement.style.height = (Board.sizeInPixels[1] / Board.size[1]) + "px";
     }
 
     const createPiece = (piece, x = 0, y = 0) => {
         let p = selectRandomPiece();
         
+        Board.grid[4][5].element.style.backgroundColor = '#00F';
+
         for(let dat of p.data) {
-            console.log(x + dat[0], y + dat[1]);
-            Board.grid[x + dat[0]][y + dat[1]] = true;
+            Board.grid[x + dat[0]][y + dat[1]] =
+                {...Board.grid[x + dat[0]][y + dat[1]], ...p};
+                console.log(Board.grid[x + dat[0]][y + dat[1]]);
         }
     }
 
     // Return random item from Pieces. Use Math.floor for uniform distribution
-    const selectRandomPiece = () => Pieces[ Math.floor( Math.random() * Pieces.length ) ];
+    const selectRandomPiece = () => {
+        const keys = Object.keys(Pieces);
+        return Pieces[ keys[ Math.floor( Math.random() * keys.length ) ] ];
+    }
 
     const initGrid = () => {
-        // Create multidimensional array from Board.size and set all values to false
-        Board.grid = Array.from(
+        Board.element.innerHTML = '';
+
+        // Create multidimensional array from Board.size and set all values to a empty objects
+        /*Board.grid = Array.from(
             Array(Board.size[0]),
-            () => new Array(Board.size[1]).fill(false)
-        );
+            () => new Array(Board.size[1]).fill( {} )
+        );*/
+
+        Board.grid = [];
+        for (let xx=0;xx<Board.size[0]; ++xx) {
+            Board.grid[xx] = [];
+            for (let yy=0;yy<Board.size[1]; ++yy) {
+                Board.grid[xx][yy] = {};
+            }
+        }
+
+
+        // Output grid contents as a filled or empty BlockChar
+        for (let yy=0; yy<Board.size[1]; ++yy) {
+            
+            for (let xx=0; xx<Board.size[0]; ++xx) {
+
+                Board.grid[xx][yy].element = Board.blockElement.cloneNode();
+
+                Board.element.appendChild( Board.grid[xx][yy].element );
+            }
+            Board.element.appendChild( document.createElement('br') );
+        }
+
+        console.log(Board.grid);
     }
 
     const drawBoard = () => {
-        Board.element.style.color = '#F00';
-        Board.element.style.fontFamily = 'Helvetica';
-        let boardString = '';
 
         // Output grid contents as a filled or empty BlockChar
         for (let yy=0; yy<Board.size[1]; ++yy) {
             for (let xx=0; xx<Board.size[0]; ++xx) {
-                boardString += (Board.grid[xx][yy] ? Board.filledBlock : Board.emptyBlock);
+                    Board.grid[xx][yy].element.style.backgroundColor = Board.grid[xx][yy].color;
             }
-            boardString += '<br>';
         }
-        Board.element.innerHTML = boardString;
     }
 })()
